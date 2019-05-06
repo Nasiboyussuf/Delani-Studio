e3849203a7af1f94fbcde195d0f6fecd57700628
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-05-06 08:04:50 +0300

    add toggle in js file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".clickable1").click(function(){
+    $(".hide1").slideToggle(1200);
+    $(".show1").slideToggle(1500);
+  });
+ 
+  $(".clickable2").click(function(){
+    $(".hide2").slideToggle(1200);
+    $(".show2").slideToggle(1500);
+  });
+ 
+  $(".clickable3").click(function(){
+    $(".hide3").slideToggle(1200);
+    $(".show3").slideToggle(1500);
+  });
